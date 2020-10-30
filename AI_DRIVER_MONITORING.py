58351dbb8be735ac89b6b75b3bb14f789431e01d
ARNAB DAS <raj713335@gmail.com>
2020-10-30 19:50:01 +0530

    Initial Commit

--- AI_DRIVER_MONITORING.py
+++ AI_DRIVER_MONITORING.py
@@ -1,5 +1,6 @@
 
 #Import necessary libraries
+import os
 from scipy.spatial import distance
 from imutils import face_utils
 import numpy as np
@@ -7,6 +8,9 @@ import pygame #For playing sound
 import time
 import dlib
 import cv2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 
 #Initialize Pygame and load music
 pygame.mixer.init()
@@ -24,6 +28,62 @@ COUNTER = 0
 #Load face cascade which will be used to draw a rectangle around detected faces.
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (1200, 680),
+                                 (104.0, 177.0, 123.0))
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.55:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
 #This function calculates and return eye aspect ratio
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
@@ -40,6 +100,10 @@ predictor = dlib.shape_predictor('haarcascades/shape_predictor_68_face_landmarks
 #Extract indexes of facial landmarks for the left and right eye
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
+
+base_dir = os.getcwd()
+base_dir = base_dir.replace('\\', '/')
+model_store_dir = base_dir + '/Model/mask_detector.model'
 
 #Start webcam video capture
 video_capture = cv2.VideoCapture(0)
