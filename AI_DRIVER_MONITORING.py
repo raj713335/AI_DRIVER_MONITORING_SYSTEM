e54c645d80625fd5901886d33a5864f1e7566ce2
ARNAB DAS <raj713335@gmail.com>
2020-11-01 06:45:02 +0530

    Initial Commit

--- AI_DRIVER_MONITORING.py
+++ AI_DRIVER_MONITORING.py
@@ -29,60 +29,7 @@ COUNTER = 0
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
 
-def detect_and_predict_mask(frame, faceNet, maskNet):
-    # grab the dimensions of the frame and then construct a blob
-    # from it
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-                                 (104.0, 177.0, 123.0))
-    # pass the blob through the network and obtain the face detections
-    faceNet.setInput(blob)
-    detections = faceNet.forward()
-    # initialize our list of faces, their corresponding locations,
-    # and the list of predictions from our face mask network
-    faces = []
-    locs = []
-    preds = []
 
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
-        confidence = detections[0, 0, i, 2]
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > 0.55:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
-            face = frame[startY:endY, startX:endX]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face, (224, 224))
-            face = img_to_array(face)
-            face = preprocess_input(face)
-            # add the face and bounding boxes to their respective
-            # lists
-            faces.append(face)
-            locs.append((startX, startY, endX, endY))
-
-    # only make a predictions if at least one face was detected
-    if len(faces) > 0:
-        # for faster inference we'll make batch predictions on *all*
-        # faces at the same time rather than one-by-one predictions
-        # in the above `for` loop
-        faces = np.array(faces, dtype="float32")
-        preds = maskNet.predict(faces, batch_size=32)
-    # return a 2-tuple of the face locations and their corresponding
-    # locations
-    return (locs, preds)
 
 #This function calculates and return eye aspect ratio
 def eye_aspect_ratio(eye):
@@ -101,18 +48,9 @@ predictor = dlib.shape_predictor('haarcascades/shape_predictor_68_face_landmarks
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
-base_dir = os.getcwd()
-base_dir = base_dir.replace('\\', '/')
-model_store_dir = base_dir + '/SMOKER_DETECTOR/Model/smoking_detector.model'
-
-face_detector_caffe = base_dir + '/SMOKER_DETECTOR/face_detector/res10_300x300_ssd_iter_140000.caffemodel'
-prototxtPath = base_dir + '/SMOKER_DETECTOR/face_detector/deploy.prototxt'
-weightsPath = face_detector_caffe
-faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
-maskNet = load_model(model_store_dir)
 
 #Start webcam video capture
-video_capture = cv2.VideoCapture('sample.mp4')
+video_capture = cv2.VideoCapture(0)
 
 
 
@@ -168,26 +106,7 @@ while(True):
             pygame.mixer.music.stop()
             COUNTER = 0
 
-    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
 
-    # loop over the detected face locations and their corresponding
-    # locations
-    for (box, pred) in zip(locs, preds):
-        # unpack the bounding box and predictions
-        (startX, startY, endX, endY) = box
-        (mask, withoutMask) = pred
-        print("mask", mask, withoutMask)
-        # determine the class label and color we'll use to draw
-        # the bounding box and text
-        label = "Not Smoking" if mask > (withoutMask + 0.90) else "Smoking"
-        color = (0, 0, 255) if label == "Mask" else (0, 0, 255)
-        # include the probability in the label
-        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-        # display the label and bounding box rectangle on the output
-        # frame
-        cv2.putText(frame, label, (startX, startY - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
 
 
